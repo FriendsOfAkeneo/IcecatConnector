06048627abf4bfa15333ded4cc62d89b5bffbdc2
jmleroux <jmleroux.pro@gmail.com>
2017-11-08 23:26:27 +0100

    PIM 2.0 migration: fix Travis

--- bin/setup.bash
+++ bin/setup.bash
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-APP_DIR="$(readlink -e $SCRIPT_DIR/./../../../../app)"
-APP_VIEWS_DIR="$APP_DIR/Resources/PimEnrichBundle/views"
-echo $APP_DIR
-mkdir -p $APP_VIEWS_DIR/MassEditAction/product/configure
-cp ./../src/Resources/views/icecat-enrichment.html.twig $APP_VIEWS_DIR/MassEditAction/product/configure
+BIN_DIR="$(readlink -e $SCRIPT_DIR/./../../../../bin)"
 
-cd $APP_DIR
+cd $BIN_DIR
+echo $(pwd)
 ./console akeneo:batch:create-job "Akeneo Mass Edit Connector" "mass_edit_icecat_enrichment" "mass_edit" "mass_edit_icecat_enrichment"
