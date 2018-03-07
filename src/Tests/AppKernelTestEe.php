2b445e4668f1c9c72a305512efa8271cb382c326
Mathias Métayer <mathias.metayer@akeneo.com>
2018-03-07 17:49:31 +0100

    Fix class header

--- src/Tests/AppKernelTestEe.php
+++ src/Tests/AppKernelTestEe.php
@@ -5,7 +5,7 @@ namespace Pim\Bundle\IcecatConnectorBundle\Tests;
 /**
  * App kernel for the integration tests.
  *
- * @author    Alexandre Hocquard <alexandre.hocquard@akeneo.com>
+ * @author    Mathias Métayer <mathias.metayer@akeneo.com>
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
